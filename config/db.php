7723b46e38b5ed4372a75875f1ad5940a89bc8f9
notserpp <prestonsegura@yahoo.com>
2019-01-10 13:33:12 -0800

    make it better

--- config/db.php
+++ config/db.php
@@ -1,6 +1,6 @@
 <?php
     //DB params
-    define("DB_HOST", "127.0.0.1");
-    define("DB_USER", "root");
-    define("DB_PASS", "");
-    define("DB_NAME", "paypage");
+    define("DB_HOST", "us-cdbr-iron-east-01.cleardb.net");
+    define("DB_USER", "b6f1127ae9fa9d");
+    define("DB_PASS", "8854e9b8");
+    define("DB_NAME", "heroku_b9c5165b9a10d2a");
